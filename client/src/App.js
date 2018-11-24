133f6ca9d67bbfd64245787896d7a54c68b7a150
bartosz-paternoga <bpaternoga@hotmail.com>
2018-11-24 11:32:48 +0100

    4th

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,8 @@
 
 
       onChange = (e) => {
-        switch (e.target.name) {
-          case 'selectedFile':
-            this.setState({ selectedFile: e.target.files[0] });
-            break;
-          default:
-            this.setState({ [e.target.name]: e.target.value });
-        }
+            this.setState({ selectedFile: e.target.files });
+
       }
 
 
